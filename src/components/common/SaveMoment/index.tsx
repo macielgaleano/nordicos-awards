67bb90f53b24fadd8a038cb0555bf8093cd355f0
Maciel Galeano <46730330+macielgaleano@users.noreply.github.com>
2025-01-12 14:12:51 -0300

    Home

---
+++ src/components/common/SaveMoment/index.tsx
@@ -0,0 +1,13 @@
+import { Save } from "lucide-react";
+import "./index.css";
+
+const SaveMoment = () => {
+  return (
+    <div className="tag save-moment">
+      <span>Guardar</span>
+      <Save size={36} />
+    </div>
+  );
+};
+
+export default SaveMoment;
